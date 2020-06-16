8efbd3badd187a7ea8701f66006888663c8d8492
AramburuMerlos <40673171+AramburuMerlos@users.noreply.github.com>
2020-06-16 11:05:12 -0700

    fix url-checked date

--- 00_GetCDL.R
+++ 00_GetCDL.R
@@ -2,7 +2,7 @@
 
 # years to download 
 yy <- formatC(08:17, width = 2, format = "d", flag = "0")
-# url (checked on June 2020)
+# url (checked on 6/16/2020)
 urls <- paste0("ftp://ftp.nass.usda.gov/download/res/20", yy, "_30m_cdls.zip")
 # file names. Saved in D: (outside Rproject)
 fns <- paste0("D:/CDL/20", yy, "_30m_cdls.zip")
