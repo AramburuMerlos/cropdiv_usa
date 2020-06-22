66638021df473415d49b0473ea96c112cd77315d
Fernando Aramburu Merlos <faramburumerlos@gmail.com>
2020-06-22 09:27:35 -0700

    add perennials masked

--- 06_AggrTemporal.R
+++ 06_AggrTemporal.R
@@ -1,14 +1,14 @@
 # Aggregate Temporal Diversity Estimations. 
 # (by exp(mean of log(D)) or exp(mean(H))
 
-library(raster)
+library(raster) 
 library(data.table)
 
 source('functions/Aggr.R')
 source('functions/BigAggrTmp.R')
 
 Tpath <- "D:/TmpDivData"
-Dt = raster("D:/TmpDivData/TmpDiv.tif")
+Dt = raster(file.path(Tpath,"TmpDiv.tif"))
 
 # aggregation factors and final resolutions
 facts <- c(11 * 1:6, 33 * 2^(2:11))
@@ -40,3 +40,20 @@ for (i in bf){
        progress = 'text', overwrite = TRUE)
 }
 
+# Perennials Masked ------
+PMpath <- "D:/PerennialMask"
+Dt_pm <- raster(file.path(PMpath,"TmpDivPM.tif"))
+
+facts_pm <- c(33 * 2^(0:2)) #only 1, 2 and 4km
+(res_m = facts_pm * 30)
+ndig = max(nchar(res_m))
+outnames_pm = paste0('Dt_',
+                     formatC(res_m, width = ndig, flag = 0, format = 'd'),
+                     'm_PM.tif')
+
+for (i in 1:length(facts_pm)){
+  aggr(Dt_pm, fact = facts_pm[i], fun = function(x, ...)mymean(x),
+       filename = file.path(PMpath, outnames_pm[i]),
+       options = c("COMPRESS=Deflate","PREDICTOR=1","ZLEVEL=6"),
+       progress = 'text', overwrite = TRUE)
+}
