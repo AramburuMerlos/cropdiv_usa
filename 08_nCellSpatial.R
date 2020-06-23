2efd22d538d243fbd829549a28304c69d5cfe3ce
Fernando Aramburu Merlos <faramburumerlos@gmail.com>
2020-06-23 09:06:52 -0700

    tested

---
+++ 08_nCellSpatial.R
@@ -0,0 +1,53 @@
+library(raster)
+library(data.table)
+
+Rpath <- "D:/ReclMaskedData"
+rnames <- Sys.glob(file.path(Rpath, '*.tif'))
+rl <- lapply(rnames, raster)
+names(rl) <- rnames
+
+source("Functions/Aggr.R")
+source("Functions/BigAggrN.R")
+
+npath <- 'D:/nCellSpatial'
+dir.create(npath)
+
+# aggregation factors and final resolutions
+facts <- c(11 * 1:6, 33 * 2^(2:11))
+(res_m <- facts * 30)
+
+# output names
+ndig <- max(nchar(res_m))
+res_m <- paste0(formatC(res_m, 
+                        width = ndig, 
+                        flag = 0, 
+                        format = 'd'), 'm_')
+outnames <- expand.grid('n_', 2008:2017, res_m, ".tif", 
+                       stringsAsFactors = FALSE, KEEP.OUT.ATTRS = FALSE)
+dc_args <- c(outnames[c(1,3,2,4)], sep="")
+outnames <- do.call(paste, dc_args)
+
+# factors to use with aggr (small factors) and bigaggr (big factors)
+sf <- which(facts < 1000) # threshold should depend on RAM
+bf <- which(facts >= 1000)
+
+# Small Factors ----
+# loop for each year inside loop for each aggregation factor
+for (f in sf){   
+  for (i in 1:10){
+    aggr(rl[[i]], fact = facts[f], fun = function(x, ...) sum(!is.na(x)),
+         filename = file.path(npath, outnames[(f-1)*10+i]), 
+         progress = 'text', NAflag = 0, overwrite = TRUE)
+  }
+}
+
+# Big Factors ---- 
+# loop for each year inside loop for each aggregation factor
+for (f in bf){   
+  for (i in 1:10){
+    BigAggrN(rl[[i]], fact = facts[f], 
+         filename = file.path(npath, outnames[(f-1)*10+i]),
+         options = c("COMPRESS=Deflate","PREDICTOR=1","ZLEVEL=6"),
+         progress = 'text', NAflag = 0, overwrite = TRUE)
+  }
+}
